dfdc9fb0a1bc5b250c45ac5e634aae1f31a5a0aa
Иван Сабиров <sabirivan@yandex.com>
2023-11-28 10:28:34 +0600

    Update files

---
+++ src/config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Config;
+
+const CONFIGS =  [
+    'BOT_NAME' => '',
+    'BOT_TOKEN' => '',
+    'OPEN_WEATHER_MAP_API_KEY' => ''
+];
--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Config;
-
-const CONFIGS =  [
-    'BOT_NAME' => 'exampleProjectTest_bot',
-    'BOT_TOKEN' => '6939906424:AAFAkI12mNyrSCaxE54XFC6dH983cHTsC4M',
-    'OPEN_WEATHER_MAP_API_KEY' => 'ecbc17f000b77e3bde03ca3c11454105'
-];
